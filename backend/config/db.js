c5d6cff5898c4d5030e5547cfcf576b418b4f314
Akshansh Dubey <98404936+Akshansh97@users.noreply.github.com>
2026-01-14 00:37:31 +0530

    Update MongoDB connection URI in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //connectDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://adubey:a123@cluster0.albej3z.mongodb.net/?appName=Cluster0');
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://uname:pw@cluster0.albej3z.mongodb.net/?appName=Cluster0');
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
